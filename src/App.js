49918433c93ecc509abce2e8af6367bf9543893d
Thiago <throcha3@gmail.com>
2022-11-25 18:22:07 -0300

    setup game component and css

--- src/App.js
+++ src/App.js
@@ -23,7 +23,30 @@ function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
 
+  const [pickedWord, setPickedWord] = useState("");
+  const [pickedCategory, setPickedCategory] = useState("");
+  const [letters, setLetters] = useState("");
+
+  const pickWordAndCategory  = () => {
+    const categories = Object.keys(words);
+    const category = categories[Math.floor(Math.random() * Object.keys(categories).length)];
+
+    const word = words[category][Math.floor(Math.random() *  words[category].length)];
+
+    return {word, category};
+  }
+
+
+
   const startGame = () => {
+    const {word, category} = pickWordAndCategory();
+
+    let wordLetters = word.split("");
+
+    wordLetters = wordLetters.map((l) => l.toLowerCase());
+
+    console.log(wordLetters);
+
     setGameStage(stages[1].name);
   }
 
--- src/components/Game.js
+++ src/components/Game.js
@@ -2,9 +2,32 @@ import "./Game.css";
 
 const Game = ({verifyLetter}) => {
   return (
-    <div>
-      <h1>Game</h1>
-      <button onClick={verifyLetter}>Finalizar Jogo</button>
+    <div className="game">
+      <p className="points">
+        <span>Points: 000</span>
+      </p>
+      <h1>Guess the word:</h1>
+      <h3 className="tip">
+        Tip about the word: <span>Tip...</span>
+      </h3>
+      <p>You still have XX tries</p>
+      <div className="wordContainer">
+        <span className="letter">A</span>
+        <span className="blankSquare"></span>
+      </div>
+
+      <div className="letterContainer">
+        <p>Try to guess a letter of the word:</p>
+        <form>
+          <input type="text" name="letter" maxLength="1" required/>
+          <button>Play!</button>
+        </form>
+      </div>
+      <div className="wrongLettersContainer">
+        <p>Already tried letters:</p>
+        <span>a,</span>
+        <span>t,</span>
+      </div>
     </div>
   )
 }
