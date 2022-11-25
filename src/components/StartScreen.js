8372a50b03e8e370bc19330ca1da728c9ff24491
Thiago <throcha3@gmail.com>
2022-11-25 16:37:09 -0300

    initial project configs

---
+++ src/components/StartScreen.js
@@ -0,0 +1,11 @@
+import "./StartScreen.css";
+
+const StartScreen = () => {
+    return <div className="start">
+        <h1> Secret World</h1>
+        <p> Clique no botão abaixo para iniciar o jogo</p>
+        <button>Começar Jogo</button>
+    </div>;
+}
+
+export default StartScreen;
